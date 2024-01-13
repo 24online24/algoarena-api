a837bc864270c08cef66bf5aee76bdadf1b99d40
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-01-13 14:39:46 +0200

    Initial commit

---
+++ src/main/java/org/judy/algoarena/AlgoarenaApiApplication.java
@@ -0,0 +1,13 @@
+package org.judy.algoarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoarenaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoarenaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/judy/algoarena/AlgoarenaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.judy.algoarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoarenaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
