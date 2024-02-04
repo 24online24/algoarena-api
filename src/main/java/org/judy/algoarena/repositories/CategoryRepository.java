d817f6ffe1709eda66e928c9532304abd902fa1e
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-02-04 16:34:56 +0200

    Add category and problem set repositories

---
+++ src/main/java/org/judy/algoarena/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package org.judy.algoarena.repositories;
+
+import org.judy.algoarena.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
---
+++ src/main/java/org/judy/algoarena/repositories/ProblemSetRepository.java
@@ -0,0 +1,7 @@
+package org.judy.algoarena.repositories;
+
+import org.judy.algoarena.models.ProblemSet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProblemSetRepository extends CrudRepository<ProblemSet, Long> {
+}
